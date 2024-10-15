8f6cf0f9679e8c0fb6e3cfb6a379f77d16b2a8f6
Gromowlad88 <artur.balicki@me.com>
2024-10-15 11:00:42 +0200

    Add files via upload

---
+++ zadanie1.R
@@ -0,0 +1,18 @@
+1.  **Uruchom RStudio** na swoim komputerze.
+
+2.  **Zapoznaj się z interfejsem użytkownika:**
+  
+  3.  **Stwórz nowy skrypt R:**
+  
+  4.  **W skrypcie napisz komentarze** opisujące funkcje poszczególnych paneli.
+#Source (Edytor skrytów ) - Służy do wpisywania skrytów, których funkcje nie są automatycznie wykonywane
+#Konsola - Służy do egzekwowania podstawowych skryptów i wyświetlania 
+#Środowisko - Służy do wyświetlania wszystkich zdefiniowanych pakietów
+#Output panel - Zawiera pakiety, wykresy i inne narzędzia pomagające w korzystaniu z RStudio 
+5.  **Znajdź w RStudio ustawienia** i **zmień motyw kolorystyczny** na taki, który najbardziej Ci odpowiada.
+
+6.  **Napisz w komentarzu**, jaki motyw wybrałeś/aś i dlaczego.
+#Ponieważ nie da się zaimportować Comic Sansa jako główny font
+7.  **Zapisz skrypt** jako `zadanie1.R` w swoim folderze.
+
+8.  **Prześlij plik do swojego repozytorium na GitHub**.
